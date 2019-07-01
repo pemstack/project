dd13454745c5d745c393dbf5e3af21460d97e5fe
Edon Gashi <edon.gashi@uni-pr.edu>
2019-07-02 00:27:26 +0200

    antd icons workaround

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,15 @@
 const { override, fixBabelImports } = require('customize-cra')
+
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
-  })
+  }),
+  function (config, env) {
+    const alias = (config.resolve.alias || {})
+    alias['@ant-design/icons'] = 'purched-antd-icons'
+    config.resolve.alias = alias
+    return config
+  }
 )
