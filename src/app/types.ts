78ed9942a5cf6c55e58e05a671e5dbca0958d508
Edon Gashi <edon.gashi@uni-pr.edu>
2019-07-02 23:20:29 +0200

    improve typings

--- src/app/types.ts
+++ src/app/types.ts
@@ -2,7 +2,7 @@ import { AppExtension, AppNode, Services } from '@pema/app'
 import { ActionParams, Controller as RouteController, Router, View as RouteView } from '@pema/router'
 import { JValue } from '@pema/utils'
 import { DefaultLayoutProps } from 'app/layout/DefaultLayout'
-import { FunctionComponent } from 'react'
+import { ComponentType } from 'react'
 
 export interface App extends AppNode {
   router: Router
@@ -29,26 +29,34 @@ interface LayoutRouteView<TProps extends RouteProps> extends RouteView<TProps> {
   layout?: LayoutPicker<TProps>
 }
 
-export type View<TProps extends RouteProps = RouteProps> =
-  FunctionComponent<TProps> & LayoutRouteView<TProps>
+export type View<TProps = {}> =
+  ComponentType<RouteProps & TProps> & LayoutRouteView<RouteProps & TProps>
+
+export type PropsOf<TView> =
+  TView extends LayoutRouteView<infer TProps> ? TProps : RouteProps
 
 export
   interface Controller<TProps extends RouteProps = RouteProps>
   extends RouteController<TProps> { }
 
-export type WithController<TController extends Controller> =
-  RouteProps & { readonly controller: TController }
+export interface WithController<TController extends Controller> {
+  readonly controller: TController
+}
 
-export type WithApp<TApp extends App> =
-  RouteProps & { readonly app: TApp }
+export interface WithApp<TApp extends App> {
+  readonly app: TApp
+}
 
-export type WithAppExtension<TExtension extends AppExtension> =
-  RouteProps & { readonly app: Services<TExtension> }
+export interface WithAppExtension<TExtension extends AppExtension> {
+  readonly app: Services<TExtension>
+}
 
-export type WithState<TState extends { [TKey in keyof TState]: JValue | {} }> =
-  RouteProps & { readonly state: TState }
+export interface WithState<TState extends { [TKey in keyof TState]: JValue | {} }> {
+  readonly state: TState
+}
 
-export type WithSession<TSession extends { [TKey in keyof TSession]: JValue | {} }> =
-  RouteProps & { readonly session: TSession }
+export interface WithSession<TSession extends { [TKey in keyof TSession]: JValue | {} }> {
+  readonly session: TSession
+}
 
 export default 0
