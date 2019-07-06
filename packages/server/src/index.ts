dd1e168ea0a2fe4d2ea89c4d30783ac1ee5b97b6
Edon Gashi <edon.gashi@uni-pr.edu>
2019-07-06 13:25:16 +0200

    refactor swagger init

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,20 +1,17 @@
 import { NestFactory } from '@nestjs/core'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from 'app'
+import { setupSwagger } from 'swagger'
 
 async function bootstrap() {
+  const basePath = process.env.NEST_BASE_PATH || 'api'
+
   const app = await NestFactory.create(AppModule)
-  app.setGlobalPrefix('api')
-
-  const options = new DocumentBuilder()
-    .setBasePath('api')
-    .setTitle('@my-app')
-    .setDescription('@my-app API description')
-    .setVersion('1.0')
-    .build()
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('api/docs', app, document)
-
+  app.setGlobalPrefix(basePath)
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }))
+  setupSwagger(app, basePath)
   await app.listen(4000)
 }
 
