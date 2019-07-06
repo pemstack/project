b1337f68dd520c263ff923ad3478f0301469ae56
Edon Gashi <edon.gashi@uni-pr.edu>
2019-07-06 15:54:09 +0200

    dto metadata

--- packages/server/src/modules/todos/todos.dto.ts
+++ packages/server/src/modules/todos/todos.dto.ts
@@ -2,13 +2,13 @@ import { ApiModelProperty } from '@nestjs/swagger'
 import { IsString, MinLength, IsBoolean, IsOptional } from 'class-validator'
 
 export class CreateTodoDto {
-  @ApiModelProperty()
+  @ApiModelProperty({ minLength: 1 })
   @IsString()
   @MinLength(1)
   title: string
 
+  @ApiModelProperty({ required: false, default: false })
   @IsBoolean()
   @IsOptional()
-  @ApiModelProperty()
   done?: boolean = false
 }
