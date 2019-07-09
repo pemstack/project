f8878235c5344da429a6bc6d99601e6bf4894c6e
Edon Gashi <edon.gashi@uni-pr.edu>
2019-07-09 19:08:07 +0200

    fix resolver, update packages

--- packages/server/src/modules/todos/todos.resolver.ts
+++ packages/server/src/modules/todos/todos.resolver.ts
@@ -1,6 +1,6 @@
-import { Resolver, Query } from 'type-graphql';
-import { Todo } from './todos.entity';
-import { TodosService } from './todos.service';
+import { Resolver, Query } from '@nestjs/graphql'
+import { Todo } from './todos.entity'
+import { TodosService } from './todos.service'
 
 @Resolver(of => Todo)
 export class TodosResolver {
@@ -10,7 +10,6 @@ export class TodosResolver {
 
   @Query(returns => [Todo], { nullable: true })
   async todos() {
-    console.log('todoooos')
     return await this.todosService.repository.find() || []
   }
 }
