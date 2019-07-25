9bb8813efe4ad1d7d802afe528436e2eb963b66a
Edon Gashi <edon.gashi@uni-pr.edu>
2019-07-25 20:40:48 +0200

    added antd formik manually

---
+++ packages/web/src/forms/Cascader.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { Cascader as $Cascader } from 'antd'
+import { Field, FieldProps } from 'formik'
+import { FormikFieldProps } from './FieldProps'
+import { CascaderProps as $CascaderProps } from 'antd/lib/cascader'
+
+export type CascaderProps = FormikFieldProps & $CascaderProps
+
+export const Cascader = ({ name, validate, ...restProps }: CascaderProps) => (
+  <Field name={name} validate={validate}>
+    {({ field: { value }, form: { setFieldValue, setFieldTouched } }: FieldProps) => (
+      <$Cascader
+        value={value}
+        // tslint:disable-next-line: no-shadowed-variable
+        onChange={value => {
+          setFieldValue(name, value)
+          setFieldTouched(name, true)
+        }}
+        {...restProps}
+      />
+    )}
+  </Field>
+)
