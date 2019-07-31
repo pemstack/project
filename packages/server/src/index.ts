3a5ee95d7388864ae19fc69a6080e8bd1d1cfd79
Edon Gashi <edon.gashi@uni-pr.edu>
2019-07-31 17:38:49 +0200

    add backend cors and helmet

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from 'modules/app'
 import { swaggerSetup } from 'swagger/swagger.setup'
+import helmet from 'helmet'
 
 async function bootstrap() {
   const basePath = process.env.NEST_BASE_PATH || 'api'
 
   const app = await NestFactory.create(AppModule)
-
+  app.enableCors()
+  app.use(helmet())
   app.setGlobalPrefix(basePath)
   app.useGlobalPipes(new ValidationPipe({
     transform: true
