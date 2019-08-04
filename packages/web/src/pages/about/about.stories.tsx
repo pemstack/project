59b362d5edae2fb2c0af022f15090a88fbf896bd
Edon Gashi <edon.gashi@uni-pr.edu>
2019-08-05 00:51:28 +0200

    add without layout story

--- packages/web/src/pages/about/about.stories.tsx
+++ packages/web/src/pages/about/about.stories.tsx
@@ -5,12 +5,19 @@ import { AboutView } from './about.view'
 import DefaultLayout from 'app/layout/DefaultLayout';
 
 storiesOf('AboutView', module)
-  .add('default', () => (
+  .add('complete', () => (
     <AppProvider path='/about'>
       {(props) => (
         <DefaultLayout {...props}>
           <AboutView {...props} />
         </DefaultLayout>
+      )}
+    </AppProvider>
+  ))
+  .add('without layout', () => (
+    <AppProvider path='/about'>
+      {(props) => (
+        <AboutView {...props} />
       )}
     </AppProvider>
   ))
