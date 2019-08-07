c3665b313e266a1d5bc07910115169fad2c89883
Edon Gashi <edon.gashi@uni-pr.edu>
2019-08-07 23:26:47 +0200

    delete pages

--- packages/web/src/pages/counter/index.ts
+++
@@ -1,5 +0,0 @@
-import { controller, view } from 'app'
-import { CounterController } from './counter.controller'
-import { CounterView } from './counter.view'
-
-export default controller(CounterController, view(CounterView))
--- packages/web/src/pages/todos/index.ts
+++
@@ -1,6 +0,0 @@
-import { authorize, view } from 'app'
-import { TodosView } from './todos.view'
-
-export default authorize({
-  action: view(TodosView)
-})
