bc6ca2b93c6dd2ba57474d6329d9d51dd9d79da5
Edon Gashi <edon.gashi@uni-pr.edu>
2019-08-08 01:07:40 +0200

    responsive side menu

--- packages/web/src/app/layout/DefaultLayout.tsx
+++ packages/web/src/app/layout/DefaultLayout.tsx
@@ -1,15 +1,37 @@
-import { Breadcrumb, Layout, Menu, Icon } from 'antd'
+import React, { useState, FunctionComponent } from 'react'
+import { Layout, Menu, Icon } from 'antd'
 import { RouteParams } from 'app/types'
-import React, { FunctionComponent } from 'react'
 import './DefaultLayout.css'
+
+const { Header, Content, Footer, Sider } = Layout
 
 export interface DefaultLayoutProps { }
 
+function getWidth() {
+  return typeof window !== 'undefined' && window.innerWidth
+}
+
 export const DefaultLayout: FunctionComponent<RouteParams & DefaultLayoutProps> = ({ children }) => {
+  const width = getWidth()
+  const [mobile, setMobile] = useState(width <= 576)
+  const [collapsed, setCollapsed] = useState(width <= 768)
+
+  function toggle() {
+    setCollapsed(c => !c)
+  }
+
   return (
     <Layout className='DefaultLayout'>
-      <Layout.Sider className='DefaultLayout__Sider'>
-        <Menu theme='dark' mode='inline' defaultSelectedKeys={['4']}>
+      <Sider
+        className='DefaultLayout__sider'
+        trigger={null}
+        collapsed={collapsed}
+        collapsedWidth={mobile ? 0 : undefined}
+        breakpoint='sm'
+        onBreakpoint={broken => setMobile(broken)}
+      >
+        <div className='DefaultLayout__logo' />
+        <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
           <Menu.Item key='1'>
             <Icon type='user' />
             <span className='nav-text'>nav 1</span>
@@ -27,24 +49,34 @@ export const DefaultLayout: FunctionComponent<RouteParams & DefaultLayoutProps> 
             <span className='nav-text'>nav 4</span>
           </Menu.Item>
         </Menu>
-      </Layout.Sider>
-      <Layout.Content className='DefaultLayout__Content'>
-        <div className='content'>
-          <Breadcrumb className='DefaultLayout__Breadcrumb'>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className='DefaultLayout__children-wrapper'>
-            {children}
+      </Sider>
+      <Layout className='DefaultLayout__main'>
+        <Header style={{ background: '#fff', padding: 0 }}>
+          <Icon
+            className='DefaultLayout__trigger'
+            type={collapsed ? 'menu-unfold' : 'menu-fold'}
+            onClick={toggle}
+          />
+        </Header>
+        <Content className='DefaultLayout__content'>
+          <div style={{ background: 'white' }}>
+            content
+            content
+            content
+            content
+            content
+            content
+            content
+            content
+            content
+            content
+            content
           </div>
-        </div>
-      </Layout.Content>
-      <Layout.Footer className='DefaultLayout__Footer'>
-        <div className='content'>
-          Awesome project using Ant Design
-        </div>
-      </Layout.Footer>
+        </Content>
+        <Footer className='DefaultLayout__footer'>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
     </Layout>
   )
 }
--- packages/web/src/forms/SubmitButton.tsx
+++ packages/web/src/forms/SubmitButton.tsx
@@ -5,20 +5,28 @@ import { Field, FieldProps } from 'formik'
 import React from 'react'
 import { ButtonProps } from 'antd/lib/button'
 
-export type SubmitButtonProps = ButtonProps & { onSuccess?: () => void }
+export type SubmitButtonProps = ButtonProps & {
+  onSuccess?: () => void
+  preventDisabling?: boolean
+}
 
-export const SubmitButton = ({ children, onSuccess, ...restProps }: SubmitButtonProps) => (
-  <Field>
-    {({ form: { isSubmitting, isValid } }: FieldProps) => (
-      <Button
-        loading={isSubmitting}
-        disabled={!isValid || isSubmitting}
-        type='primary'
-        htmlType='submit'
-        {...restProps}
-      >
-        {children}
-      </Button>
-    )}
-  </Field>
-)
+export const SubmitButton = ({
+  children,
+  onSuccess,
+  preventDisabling,
+  ...restProps
+}: SubmitButtonProps) => (
+    <Field>
+      {({ form: { isSubmitting, isValid } }: FieldProps) => (
+        <Button
+          loading={isSubmitting}
+          disabled={!preventDisabling && !isValid || isSubmitting}
+          type='primary'
+          htmlType='submit'
+          {...restProps}
+        >
+          {children}
+        </Button>
+      )}
+    </Field>
+  )
