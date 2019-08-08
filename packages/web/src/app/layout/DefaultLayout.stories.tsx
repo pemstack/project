8c0bea3a0593f98aa46d36b334d74580bdfe8dd2
Edon Gashi <edon.gashi@uni-pr.edu>
2019-08-08 20:51:17 +0200

    move default layout to app/layout

--- packages/web/src/app/layout/DefaultLayout.stories.tsx
+++ packages/web/src/app/layout/DefaultLayout.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { DefaultLayout } from "./DefaultLayout";
+import { DefaultLayout } from './DefaultLayout'
 
 storiesOf('DefaultLayout', module)
   .add('desktop layout', () => (
