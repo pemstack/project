4a18c41faef72c32763b6b92d4922476299ec214
Edon Gashi <edon.gashi@uni-pr.edu>
2019-08-09 22:10:04 +0200

    showCaptcha on register form

--- packages/web/src/pages/user/register/RegisterForm.tsx
+++ packages/web/src/pages/user/register/RegisterForm.tsx
@@ -8,8 +8,8 @@ const formLabelCol = {
 }
 
 const formWrapperCol = {
-    xs: { span: 24 },
-    sm: { span: 16 }
+  xs: { span: 24 },
+  sm: { span: 16 }
 }
 
 const tailWrapperCol = {
@@ -23,67 +23,67 @@ const tailWrapperCol = {
   }
 }
 
-
-  export const RegisterForm: FunctionComponent = () => {
-    return (
-      <div className='RegisterForm'>
-        <Card className='RegisterForm__card'>
-          <h2>Register</h2>
-          <Form
-            labelCol={formLabelCol}
-            wrapperCol={formWrapperCol}
-          >
-            <Form.Item
-              name='email'
-              label={
-                <span>
-                  Nickname&nbsp;
+export const RegisterForm: FunctionComponent = () => {
+  return (
+    <div className='RegisterForm'>
+      <Card className='RegisterForm__card'>
+        <h2>Register</h2>
+        <Form
+          showCaptcha
+          labelCol={formLabelCol}
+          wrapperCol={formWrapperCol}
+        >
+          <Form.Item
+            name='email'
+            label={
+              <span>
+                Nickname&nbsp;
                   <Tooltip title='What do you want others to call you?'>
-                    <Icon type='question-circle-o' />
-                  </Tooltip>
-                </span>
-              }
+                  <Icon type='question-circle-o' />
+                </Tooltip>
+              </span>
+            }
+          >
+            <Input
+              name='email'
+              prefix={<Icon type='email' style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type='text'
+              placeholder='Email'
+            />
+          </Form.Item>
+          <Form.Item name='username'>
+            <Input
+              name='username'
+              type='text'
+              prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder='Username'
+            />
+          </Form.Item>
+          <Form.Item name='password'>
+            <Input
+              name='password'
+              prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type='password'
+              placeholder='Password'
+            />
+          </Form.Item>
+          <Form.Item name='confirmPassword'>
+            <Input
+              name='confirmPassword'
+              prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type='password'
+            />
+          </Form.Item>
+          <Form.AntdItem wrapperCol={tailWrapperCol}>
+            <SubmitButton
+              preventDisabling
+              className='RegisterForm__submit'
             >
-              <Input
-                name='email'
-                prefix={<Icon type='email' style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type='text'
-                placeholder='Email'
-              />
-            </Form.Item>
-            <Form.Item name='username'>
-              <Input
-                name='username'
-                type='text'
-                prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder='Username'
-              />
-            </Form.Item>
-            <Form.Item name='password'>
-              <Input
-                name='password'
-                prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type='password'
-                placeholder='Password'
-              />
-            </Form.Item>
-            <Form.Item name='confirmPassword'>
-              <Input
-                name='confirmPassword'
-                prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type='password'
-              />
-            </Form.Item>
-            <Form.AntdItem wrapperCol={tailWrapperCol}>
-              <SubmitButton
-                preventDisabling
-                className='RegisterForm__submit'
-              >
-                Register
+              Register
               </SubmitButton>
-            </Form.AntdItem>
-          </Form>
-        </Card>
-      </div>
-    )
-  }
+          </Form.AntdItem>
+        </Form>
+      </Card>
+    </div>
+  )
+}
