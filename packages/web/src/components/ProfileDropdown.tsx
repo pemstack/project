5d0265c18c8e8dd248e6460ab4e54db22a2c8209
Edon Gashi <edon.gashi@uni-pr.edu>
2019-08-14 22:58:41 +0200

    fix dropdown crash

--- packages/web/src/components/ProfileDropdown.tsx
+++ packages/web/src/components/ProfileDropdown.tsx
@@ -1,29 +1,31 @@
 import React, { FunctionComponent } from 'react'
 import { Dropdown, Menu, Icon, Avatar } from 'antd'
+import { useTranslation } from 'react-i18next'
 
 interface ProfileDropdownProps { }
 
 export const ProfileDropdown: FunctionComponent<ProfileDropdownProps> = ({ }) => {
   return (
-    <Dropdown overlay={ProfileMenu}>
-      <Avatar size='large' icon="user" />
+    <Dropdown overlay={<ProfileMenu />}>
+      <Avatar size='large' icon='user' />
     </Dropdown>
   )
 }
 
 // TODO use LinkButton here
 export const ProfileMenu: FunctionComponent = () => {
+  const { t } = useTranslation()
   return (
     <Menu>
       <Menu.Item key='settings'>
-        <a target="_blank" rel="noopener noreferrer" href="#">
-          <Icon type="setting" /> Account Settings
+        <a target='_blank' rel='noopener noreferrer' href='#'>
+          <Icon type='setting' /> {t('user.label.settings')}
         </a>
       </Menu.Item>
       <Menu.Divider />
       <Menu.Item key='logout'>
-        <a target="_blank" rel="noopener noreferrer" href="#">
-          <Icon type="logout" /> Log Out
+        <a target='_blank' rel='noopener noreferrer' href='#'>
+          <Icon type='logout' /> {t('user.label.logout')}
         </a>
       </Menu.Item>
     </Menu>
