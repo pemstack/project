0c976ccf6efa3b18d170761912fba1c466f580be
Edon Gashi <edon.gashi@uni-pr.edu>
2019-08-14 22:55:06 +0200

    fix storybook crashes

--- packages/web/src/app/layout/DefaultLayout.stories.tsx
+++ packages/web/src/app/layout/DefaultLayout.stories.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { decorator } from 'app/mock'
 import { DefaultLayout } from './DefaultLayout'
 
 storiesOf('DefaultLayout', module)
+  .addDecorator(decorator())
   .add('desktop layout', () => (
     <DefaultLayout>
       Hello World!
