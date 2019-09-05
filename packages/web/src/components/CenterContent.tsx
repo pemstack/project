e8862b98afdfe331f170a447373aeb32d3264a86
Edon Gashi <edon.gashi@uni-pr.edu>
2019-09-05 20:39:24 +0200

    adjust center content widths

--- packages/web/src/components/CenterContent.tsx
+++ packages/web/src/components/CenterContent.tsx
@@ -3,13 +3,21 @@ import './CenterContent.css'
 
 interface CenterContentProps {
   children?: React.ReactNode
-  width?: 'small' | 'default' | 'large'
+  width?: 'small' | 'medium' | 'default' | 'large' | number
 }
 
 export const CenterContent: FunctionComponent<CenterContentProps> = ({
   children,
   width = 'default'
 }) => {
+  if (typeof width === 'number') {
+    return (
+      <div className='CenterContent' style={{ maxWidth: `${width}px` }}>
+        {children}
+      </div>
+    )
+  }
+
   return (
     <div className={`CenterContent CenterContent--${width}`}>
       {children}
