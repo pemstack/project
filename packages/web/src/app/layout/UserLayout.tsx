0b5dff8cb7dc8b69ecc03be9aa8b8181a0987fbb
ArditBaloku <arditbaloku@gmail.com>
2019-09-14 18:13:01 +0200

    Fixed user layout routing

--- packages/web/src/app/layout/UserLayout.tsx
+++ packages/web/src/app/layout/UserLayout.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FunctionComponent, Suspense } from 'react'
-import { Link } from '@pema/router-react'
+import { Link, useRouter } from '@pema/router-react'
 import { Layout, Menu, Icon } from 'antd'
 import { LanguageSelector, ProfileDropdownConnector } from 'components'
 import { Loading } from 'app/components'
@@ -9,91 +9,91 @@ import './UserLayout.css'
 const { Header, Content, Footer, Sider } = Layout
 
 export interface UserLayoutProps {
-  children?: React.ReactNode
-  defaultMobile?: boolean
-  defaultCollapsed?: boolean
+	children?: React.ReactNode
+	defaultMobile?: boolean
+	defaultCollapsed?: boolean
 }
 
 function getWidth() {
-  return typeof window !== 'undefined' && window.innerWidth
+	return typeof window !== 'undefined' && window.innerWidth
 }
 
 function fallback(value: boolean | undefined, fallbackValue: boolean) {
-  return typeof value === 'undefined'
-    ? fallbackValue
-    : value
+	return typeof value === 'undefined' ? fallbackValue : value
 }
 
 export const UserLayout: FunctionComponent<UserLayoutProps> = ({
-  children,
-  defaultMobile,
-  defaultCollapsed
+	children,
+	defaultMobile,
+	defaultCollapsed
 }) => {
-  const { t } = useTranslation()
-  const width = getWidth()
-  const [mobile, setMobile] = useState(fallback(defaultMobile, width <= 576))
-  const [collapsed, setCollapsed] = useState(fallback(defaultCollapsed, width <= 768))
+	const { t } = useTranslation()
+	const width = getWidth()
+	const [mobile, setMobile] = useState(fallback(defaultMobile, width <= 576))
+	const [collapsed, setCollapsed] = useState(
+		fallback(defaultCollapsed, width <= 768)
+	)
+	const router = useRouter()
+	const match = router.current.match
 
-  function toggle() {
-    setCollapsed(c => !c)
-  }
+	function toggle() {
+		setCollapsed(c => !c)
+	}
 
-  return (
-    <Layout className='UserLayout'>
-      <Sider
-        className='UserLayout__sider'
-        trigger={null}
-        collapsed={collapsed}
-        collapsedWidth={mobile ? 0 : undefined}
-        breakpoint='sm'
-        onBreakpoint={broken => setMobile(broken)}
-      >
-        <div className='UserLayout__logo' />
-        <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
-          <Menu.Item key='1'>
-            <Icon type='read' />
-            <span className='nav-text'>{t('Layout.label.newsfeed')}</span>
-          </Menu.Item>
-          <Menu.Item key='2'>
-            <Icon type='book' />
-            <span className='nav-text'>{t('Layout.label.courses')}</span>
-          </Menu.Item>
-          <Menu.Item key='3'>
-            <Icon type='user' />
-            <span className='nav-text'>nav 3</span>
-          </Menu.Item>
-          <Menu.Item key='4'>
-            <Icon type='user' />
-            <span className='nav-text'>nav 4</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className='UserLayout__main'>
-        <Header className='UserLayout__header'>
-          <Icon
-            className='UserLayout__trigger'
-            type={collapsed ? 'menu-unfold' : 'menu-fold'}
-            onClick={toggle}
-          />
-          <Link to='/' className='UserLayout__title'>Project name</Link>
-          <span className='UserLayout__header-profile'>
-            <ProfileDropdownConnector />
-          </span>
-          <span className='UserLayout__header-language'>
-            <LanguageSelector />
-          </span>
-        </Header>
-        <Content className='UserLayout__content'>
-          <div className='UserLayout__wrapper'>
-            <Suspense fallback={<Loading />}>
-              {children}
-            </Suspense>
-          </div>
-        </Content>
-        <Footer className='UserLayout__footer'>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
-      </Layout>
-    </Layout>
-  )
+	const selectedKeys = match.isExact ? [match.path] : []
+
+	return (
+		<Layout className='UserLayout'>
+			<Sider
+				className='UserLayout__sider'
+				trigger={null}
+				collapsed={collapsed}
+				collapsedWidth={mobile ? 0 : undefined}
+				breakpoint='sm'
+				onBreakpoint={broken => setMobile(broken)}
+			>
+				<div className='UserLayout__logo' />
+				<Menu theme='dark' mode='inline' selectedKeys={selectedKeys}>
+					<Menu.Item key='/'>
+						<Link to='/' className='nav-text'>
+							<Icon type='read' />
+							{t('Layout.label.newsfeed')}
+						</Link>
+					</Menu.Item>
+					<Menu.Item key='/courses'>
+						<Link to='/courses' className='nav-text'>
+							<Icon type='book' />
+							{t('Layout.label.courses')}
+						</Link>
+					</Menu.Item>
+				</Menu>
+			</Sider>
+			<Layout className='UserLayout__main'>
+				<Header className='UserLayout__header'>
+					<Icon
+						className='UserLayout__trigger'
+						type={collapsed ? 'menu-unfold' : 'menu-fold'}
+						onClick={toggle}
+					/>
+					<Link to='/' className='UserLayout__title'>
+						Project name
+					</Link>
+					<span className='UserLayout__header-profile'>
+						<ProfileDropdownConnector />
+					</span>
+					<span className='UserLayout__header-language'>
+						<LanguageSelector />
+					</span>
+				</Header>
+				<Content className='UserLayout__content'>
+					<div className='UserLayout__wrapper'>
+						<Suspense fallback={<Loading />}>{children}</Suspense>
+					</div>
+				</Content>
+				<Footer className='UserLayout__footer'>
+					Ant Design ©2018 Created by Ant UED
+				</Footer>
+			</Layout>
+		</Layout>
+	)
 }
--- packages/web/src/components/Poll.tsx
+++ packages/web/src/components/Poll.tsx
@@ -3,85 +3,74 @@ import { Card } from 'antd'
 import './Poll.css'
 
 export interface PollItem {
-  label: string
-  votes: number
+	label: string
+	votes: number
 }
 
 export interface PollItemProps {
-  item: PollItem
-  total: number
-  hidden?: boolean
+	item: PollItem
+	total: number
+	hidden?: boolean
 }
 
+// style={{
+//   position: 'absolute',
+//   background: '#c0e0f0',
+//   left: 0,
+//   top: 0,
+//   width: `${100 * item.votes / total}%`,
+//   height: '100%',
+//   zIndex: '1'
+// }}
 export const PollItem: FunctionComponent<PollItemProps> = ({
-  item,
-  total,
-  hidden
+	item,
+	total,
+	hidden
 }) => {
-  if (!hidden) {
-    return (
-      <Card
-        className='Card'
-        bodyStyle={{ padding: '4px 8px' }}
-        hoverable
-      >
-        <div
-          style={{
-            position: 'absolute',
-            background: '#c0e0f0',
-            left: 0,
-            top: 0,
-            width: `${100 * item.votes / total}%`,
-            height: '100%',
-            zIndex: '1'
-          }}
-        />
-        <div style={{ float: 'right' }}>
-          {item.votes}
-        </div>
-        <div style={{ position: 'absolute', zIndex: 1 }}>{item.label}</div>
-      </Card>
-    )
-  }
-  return (
-    <Card
-      className='Card'
-      bodyStyle={{ padding: '4px 8px' }}
-      hoverable
-    >
-      <div>{item.label}</div>
-    </Card>
-  )
+	if (!hidden) {
+		return (
+			<Card className='Card' bodyStyle={{ padding: '4px 8px' }} hoverable>
+				<div />
+				<div style={{ float: 'right' }}>{item.votes}</div>
+				<div style={{ position: 'absolute', zIndex: 1 }}>{item.label}</div>
+			</Card>
+		)
+	}
+	return (
+		<Card className='Card' bodyStyle={{ padding: '4px 8px' }} hoverable>
+			<div>{item.label}</div>
+		</Card>
+	)
 }
 
 export interface PollProps {
-  title?: string
-  items: PollItem[]
-  Submitted?: boolean
+	title?: string
+	items: PollItem[]
+	Submitted?: boolean
 }
 
 export const Poll: FunctionComponent<PollProps> = ({
-  title,
-  items,
-  Submitted
+	title,
+	items,
+	Submitted
 }) => {
-  const total = items.reduce((sum, current) => sum + current.votes, 0)
-  if (Submitted) {
-    return (
-      <div className='Poll'>
-        {title && <h3>{title}</h3>}
-        {items.map((item, index) => (
-          <PollItem key={index} item={item} total={total} />
-        ))}
-      </div>
-    )
-  }
-  return (
-    <div className='Poll'>
-      {title && <h3>{title}</h3>}
-      {items.map((item, index) => (
-        <PollItem key={index} item={item} total={total} hidden />
-      ))}
-    </div>
-  )
+	const total = items.reduce((sum, current) => sum + current.votes, 0)
+	if (Submitted) {
+		return (
+			<div className='Poll'>
+				{title && <h3>{title}</h3>}
+				{items.map((item, index) => (
+					<PollItem key={index} item={item} total={total} />
+				))}
+			</div>
+		)
+	}
+	return (
+		<div className='Poll'>
+			{title && <h3>{title}</h3>}
+			{items.map((item, index) => (
+				<PollItem key={index} item={item} total={total} hidden />
+			))}
+		</div>
+	)
 }
