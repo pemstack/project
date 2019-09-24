6542927356778a2d2bf24395c4f1c685f75c7197
ArditBaloku <arditbaloku@gmail.com>
2019-09-24 20:14:03 +0200

    Display attachments

--- packages/web/src/pages/courses/CoursePage.tsx
+++ packages/web/src/pages/courses/CoursePage.tsx
@@ -3,6 +3,7 @@ import { useQuery } from 'app'
 import { Markdown, CollapseCard } from 'components'
 import classNames from 'classnames'
 import { GET_COURSE_PAGE } from './courses.api'
+import { Icon } from 'antd'
 
 export interface CoursePageProps {
   courseId: string
@@ -20,6 +21,21 @@ export const CoursePage: FunctionComponent<CoursePageProps> = ({
     <div className={classNames('CoursePage', 'CoursePage--content', className)}>
       <CollapseCard>
         <Markdown source={page.content} />
+        {page.files.length > 0 && (
+          console.log('files are here'),
+          <>
+            <h3>Attachments</h3>
+            <span className='ant-upload-list ant-upload-list-text'>
+              {page.files.map(f =>
+                <span className="ant-upload-list-item-name" title={f.uid}>
+                  <Icon type="paper-clip" />
+                  {f.name}
+                </span>
+              )}
+            </span>
+          </>
+        )
+        }
       </CollapseCard>
     </div>
   )
