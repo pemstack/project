66052aa9429dfafd837d569ce7f5afd5e173e5cd
Ardit Baloku <43915132+ArditBaloku@users.noreply.github.com>
2019-10-15 15:06:36 +0200

    Landing page rework

--- packages/web/src/pages/home/LandingPage.tsx
+++ packages/web/src/pages/home/LandingPage.tsx
@@ -9,19 +9,13 @@ export const LandingPage: FunctionComponent = () => {
   const { t } = useTranslation()
   return (
     <div className='LandingPage'>
-      <Row gutter={10} className='center'>
-        <Col span={12} xs={20} sm={12} md={12} lg={12} className='Welcome'>
-          <h2>{t('LandingPage.title')}</h2>
-          <p>{t('LandingPage.text')}</p>
-          <p><strong>{t('LandingPage.alpha')}</strong></p>
-          <LinkButton to='/user/login' className='LandingPage__button'>
-            {t('LandingPage.getStarted')}
-          </LinkButton>
-        </Col>
-        <Col span={12} xs={4} sm={12} md={12} lg={12} className='Illustration'>
-          <img src={Team} width='450' height='400' />
-        </Col>
-      </Row>
+      <h2>{t('LandingPage.title')}</h2>
+      <img src={Team} width='450' height='400' />
+      <p>{t('LandingPage.text')}</p>
+      <p><strong>{t('LandingPage.alpha')}</strong></p>
+      <LinkButton to='/user/login' className='LandingPage__button'>
+        {t('LandingPage.getStarted')}
+      </LinkButton>
     </div>
   )
 }
