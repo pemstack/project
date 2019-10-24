6de4b82584904e1fcc2cd723b6a1ada752951c73
Edon Gashi <edon.gashi@uni-pr.edu>
2019-10-24 17:06:53 +0200

    made landing page responsive

--- packages/web/src/pages/home/LandingPage.tsx
+++ packages/web/src/pages/home/LandingPage.tsx
@@ -1,21 +1,40 @@
+import { Col, Row } from 'antd'
+import { LinkButton } from 'components'
+import { useBodyClass } from 'hooks'
 import React, { FunctionComponent } from 'react'
-import Team from './misc/team.png'
-import { Row, Col } from 'antd';
+import { useTranslation } from 'react-i18next'
 import './LandingPage.css'
-import { LinkButton } from 'components';
-import { useTranslation } from 'react-i18next';
+import Team from './misc/team.png'
 
 export const LandingPage: FunctionComponent = () => {
   const { t } = useTranslation()
+  useBodyClass('LandingPage')
   return (
     <div className='LandingPage'>
-      <h2>{t('LandingPage.title')}</h2>
-      <img src={Team} width='450' height='400' />
-      <p>{t('LandingPage.text')}</p>
-      <p><strong>{t('LandingPage.alpha')}</strong></p>
-      <LinkButton to='/user/login' className='LandingPage__button'>
-        {t('LandingPage.getStarted')}
-      </LinkButton>
+      <Row type='flex' align='middle' justify='center'>
+        <Col xs={22} md={12}>
+          <h2>{t('LandingPage.title')}</h2>
+          <p className='LandingPage__description'>{t('LandingPage.text')}</p>
+          <LinkButton
+            type='danger'
+            size='large'
+            to='/user/login'
+            className='LandingPage__button'
+          >
+            {t('LandingPage.getStarted')}
+          </LinkButton>
+        </Col>
+        <Col xs={22} md={10}>
+          <div className='LandingPage__img-wrapper'>
+            <img className='LandingPage__img' src={Team} />
+          </div>
+        </Col>
+      </Row>
+      <Row type='flex' align='middle' justify='center'>
+        <Col xs={22} md={12}>
+          <p className='LandingPage__alpha'>{t('LandingPage.alpha')}</p>
+        </Col>
+      </Row>
     </div>
   )
 }
